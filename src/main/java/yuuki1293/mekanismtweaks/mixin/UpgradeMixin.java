375b6e8576c3f9c92910e43853727e5ec5b80048
yuuki1293 <yuuki1293@outlook.jp>
2024-11-13 15:59:46 +0900

    fix disappear upgrades on load server

--- src/main/java/yuuki1293/mekanismtweaks/mixin/UpgradeMixin.java
+++ src/main/java/yuuki1293/mekanismtweaks/mixin/UpgradeMixin.java
@@ -2,8 +2,7 @@ package yuuki1293.mekanismtweaks.mixin;
 
 import mekanism.api.Upgrade;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import yuuki1293.mekanismtweaks.Config;
 
@@ -15,7 +14,13 @@ public abstract class UpgradeMixin {
         switch (thisObj) {
             case SPEED -> cir.setReturnValue(Config.speedStackSize);
             case ENERGY -> cir.setReturnValue(Config.energyStackSize);
-            default -> {}
+            default -> {
+            }
         }
+    }
+
+    @ModifyArg(method = "buildMap", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;clamp(III)I"), index = 2)
+    private static int injectedMaxStack(int maxStack) {
+        return Integer.MAX_VALUE;
     }
 }
