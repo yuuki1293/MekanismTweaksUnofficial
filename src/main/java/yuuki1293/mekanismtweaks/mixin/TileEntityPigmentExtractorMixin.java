f6c6697a8ea120c692091dadc4ccbdad5c3c5676
yuuki1293 <yuuki1293@outlook.jp>
2024-11-13 22:24:50 +0900

    multi process Pigment Extractor

---
+++ src/main/java/yuuki1293/mekanismtweaks/mixin/TileEntityPigmentExtractorMixin.java
@@ -0,0 +1,39 @@
+package yuuki1293.mekanismtweaks.mixin;
+
+import mekanism.api.Upgrade;
+import mekanism.api.providers.IBlockProvider;
+import mekanism.api.recipes.ItemStackToPigmentRecipe;
+import mekanism.api.recipes.cache.CachedRecipe;
+import mekanism.common.tile.base.TileEntityMekanism;
+import mekanism.common.tile.machine.TileEntityPigmentExtractor;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import yuuki1293.mekanismtweaks.Utils;
+
+// Pigment Extractor
+@Mixin(value = TileEntityPigmentExtractor.class, remap = false)
+public abstract class TileEntityPigmentExtractorMixin extends TileEntityMekanism {
+    @Unique
+    private int mekanismtweaks$baselineMaxOperations = 1;
+
+    public TileEntityPigmentExtractorMixin(IBlockProvider blockProvider, BlockPos pos, BlockState state) {
+        super(blockProvider, pos, state);
+    }
+
+    @Inject(method = "createNewCachedRecipe(Lmekanism/api/recipes/ItemStackToPigmentRecipe;I)Lmekanism/api/recipes/cache/CachedRecipe;", at = @At(value = "TAIL"))
+    private void createNewCachedRecipe(@NotNull ItemStackToPigmentRecipe recipe, int cacheIndex, CallbackInfoReturnable<CachedRecipe<ItemStackToPigmentRecipe>> cir) {
+        cir.getReturnValue().setBaselineMaxOperations(() -> mekanismtweaks$baselineMaxOperations);
+    }
+
+    @Override
+    public void recalculateUpgrades(Upgrade upgrade) {
+        super.recalculateUpgrades(upgrade);
+        mekanismtweaks$baselineMaxOperations = Utils.setBaselineMaxOperation(100, this, upgrade);
+    }
+}
