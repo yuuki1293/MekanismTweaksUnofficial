e7e628f72d0808e76c16709010d181ce257cd7b5
yuuki1293 <yuuki1293@outlook.jp>
2024-11-13 22:15:36 +0900

    multi process Compressing, Injecting, Purifying

---
+++ src/main/java/yuuki1293/mekanismtweaks/mixin/TileEntityItemStackGasToItemStackFactoryMixin.java
@@ -0,0 +1,43 @@
+package yuuki1293.mekanismtweaks.mixin;
+
+import mekanism.api.Upgrade;
+import mekanism.api.providers.IBlockProvider;
+import mekanism.api.recipes.ItemStackGasToItemStackRecipe;
+import mekanism.api.recipes.MekanismRecipe;
+import mekanism.api.recipes.cache.CachedRecipe;
+import mekanism.common.tile.factory.TileEntityFactory;
+import mekanism.common.tile.factory.TileEntityItemStackGasToItemStackFactory;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import yuuki1293.mekanismtweaks.Utils;
+
+import java.util.List;
+import java.util.Set;
+
+// Compressing, Injecting, Purifying
+@Mixin(value = TileEntityItemStackGasToItemStackFactory.class, remap = false)
+public abstract class TileEntityItemStackGasToItemStackFactoryMixin extends TileEntityFactory<MekanismRecipe> {
+    @Unique
+    private int mekanismtweaks$baselineMaxOperations = 1;
+
+    protected TileEntityItemStackGasToItemStackFactoryMixin(IBlockProvider blockProvider, BlockPos pos, BlockState state, List<CachedRecipe.OperationTracker.RecipeError> errorTypes, Set<CachedRecipe.OperationTracker.RecipeError> globalErrorTypes) {
+        super(blockProvider, pos, state, errorTypes, globalErrorTypes);
+    }
+
+    @Inject(method = "createNewCachedRecipe(Lmekanism/api/recipes/ItemStackGasToItemStackRecipe;I)Lmekanism/api/recipes/cache/CachedRecipe;", at = @At(value = "TAIL"))
+    private void createNewCachedRecipe(@NotNull ItemStackGasToItemStackRecipe recipe, int cacheIndex, CallbackInfoReturnable<CachedRecipe<ItemStackGasToItemStackRecipe>> cir) {
+        cir.getReturnValue().setBaselineMaxOperations(() -> mekanismtweaks$baselineMaxOperations);
+    }
+
+    @Override
+    public void recalculateUpgrades(Upgrade upgrade) {
+        super.recalculateUpgrades(upgrade);
+        mekanismtweaks$baselineMaxOperations = Utils.setBaselineMaxOperation(BASE_TICKS_REQUIRED, this, upgrade);
+    }
+}
