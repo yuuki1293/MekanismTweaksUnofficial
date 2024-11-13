371764c8ea909ee569588835236bc792d7d35fd6
yuuki1293 <yuuki1293@outlook.jp>
2024-11-13 22:21:52 +0900

    multi process Nutritional Liquifier

---
+++ src/main/java/yuuki1293/mekanismtweaks/mixin/TileEntityNutritionalLiquifierMixin.java
@@ -0,0 +1,39 @@
+package yuuki1293.mekanismtweaks.mixin;
+
+import mekanism.api.Upgrade;
+import mekanism.api.providers.IBlockProvider;
+import mekanism.api.recipes.ItemStackToFluidRecipe;
+import mekanism.api.recipes.cache.CachedRecipe;
+import mekanism.common.tile.base.TileEntityMekanism;
+import mekanism.common.tile.machine.TileEntityNutritionalLiquifier;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import yuuki1293.mekanismtweaks.Utils;
+
+// Nutritional Liquifier
+@Mixin(value = TileEntityNutritionalLiquifier.class, remap = false)
+public abstract class TileEntityNutritionalLiquifierMixin extends TileEntityMekanism {
+    @Unique
+    private int mekanismtweaks$baselineMaxOperations = 1;
+
+    public TileEntityNutritionalLiquifierMixin(IBlockProvider blockProvider, BlockPos pos, BlockState state) {
+        super(blockProvider, pos, state);
+    }
+
+    @Inject(method = "createNewCachedRecipe(Lmekanism/api/recipes/ItemStackToFluidRecipe;I)Lmekanism/api/recipes/cache/CachedRecipe;", at = @At(value = "TAIL"))
+    private void createNewCachedRecipe(@NotNull ItemStackToFluidRecipe recipe, int cacheIndex, CallbackInfoReturnable<CachedRecipe<ItemStackToFluidRecipe>> cir) {
+        cir.getReturnValue().setBaselineMaxOperations(() -> mekanismtweaks$baselineMaxOperations);
+    }
+
+    @Override
+    public void recalculateUpgrades(Upgrade upgrade) {
+        super.recalculateUpgrades(upgrade);
+        mekanismtweaks$baselineMaxOperations = Utils.setBaselineMaxOperation(100, this, upgrade);
+    }
+}
