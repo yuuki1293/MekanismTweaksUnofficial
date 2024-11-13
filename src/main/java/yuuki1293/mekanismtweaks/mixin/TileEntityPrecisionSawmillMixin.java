187892485b4f2ee1d641e70ea850d81f8835a3c3
yuuki1293 <yuuki1293@outlook.jp>
2024-11-13 22:25:51 +0900

    multi process Precision Sawmill

---
+++ src/main/java/yuuki1293/mekanismtweaks/mixin/TileEntityPrecisionSawmillMixin.java
@@ -0,0 +1,39 @@
+package yuuki1293.mekanismtweaks.mixin;
+
+import mekanism.api.Upgrade;
+import mekanism.api.providers.IBlockProvider;
+import mekanism.api.recipes.SawmillRecipe;
+import mekanism.api.recipes.cache.CachedRecipe;
+import mekanism.common.tile.base.TileEntityMekanism;
+import mekanism.common.tile.machine.TileEntityPrecisionSawmill;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import yuuki1293.mekanismtweaks.Utils;
+
+// Precision Sawmill
+@Mixin(value = TileEntityPrecisionSawmill.class, remap = false)
+public abstract class TileEntityPrecisionSawmillMixin extends TileEntityMekanism {
+    @Unique
+    private int mekanismtweaks$baselineMaxOperations = 1;
+
+    public TileEntityPrecisionSawmillMixin(IBlockProvider blockProvider, BlockPos pos, BlockState state) {
+        super(blockProvider, pos, state);
+    }
+
+    @Inject(method = "createNewCachedRecipe(Lmekanism/api/recipes/SawmillRecipe;I)Lmekanism/api/recipes/cache/CachedRecipe;", at = @At(value = "TAIL"))
+    private void createNewCachedRecipe(@NotNull SawmillRecipe recipe, int cacheIndex, CallbackInfoReturnable<CachedRecipe<SawmillRecipe>> cir) {
+        cir.getReturnValue().setBaselineMaxOperations(() -> mekanismtweaks$baselineMaxOperations);
+    }
+
+    @Override
+    public void recalculateUpgrades(Upgrade upgrade) {
+        super.recalculateUpgrades(upgrade);
+        mekanismtweaks$baselineMaxOperations = Utils.setBaselineMaxOperation(200, this, upgrade);
+    }
+}
