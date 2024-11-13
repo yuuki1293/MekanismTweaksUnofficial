fbfd4eb6729b47fe14920a5546f34cecf9ece9cb
yuuki1293 <yuuki1293@outlook.jp>
2024-11-13 22:01:41 +0900

    multi process Chemical Crystallizer

---
+++ src/main/java/yuuki1293/mekanismtweaks/mixin/TileEntityChemicalCrystallizerMixin.java
@@ -0,0 +1,41 @@
+package yuuki1293.mekanismtweaks.mixin;
+
+import mekanism.api.Upgrade;
+import mekanism.api.providers.IBlockProvider;
+import mekanism.api.recipes.ChemicalCrystallizerRecipe;
+import mekanism.api.recipes.ItemStackGasToItemStackRecipe;
+import mekanism.api.recipes.cache.CachedRecipe;
+import mekanism.common.tile.base.TileEntityMekanism;
+import mekanism.common.tile.machine.TileEntityChemicalCrystallizer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import yuuki1293.mekanismtweaks.Utils;
+
+// Chemical Crystallizer
+@Mixin(value = TileEntityChemicalCrystallizer.class, remap = false)
+public abstract class TileEntityChemicalCrystallizerMixin extends TileEntityMekanism {
+    @Unique
+    private int mekanismtweaks$baselineMaxOperations = 1;
+
+    public TileEntityChemicalCrystallizerMixin(IBlockProvider blockProvider, BlockPos pos, BlockState state) {
+        super(blockProvider, pos, state);
+    }
+
+    @Inject(method = "createNewCachedRecipe(Lmekanism/api/recipes/ChemicalCrystallizerRecipe;I)Lmekanism/api/recipes/cache/CachedRecipe;", at = @At(value = "TAIL"))
+    private void createNewCachedRecipe(@NotNull ChemicalCrystallizerRecipe recipe, int cacheIndex, CallbackInfoReturnable<CachedRecipe<ChemicalCrystallizerRecipe>> cir) {
+        cir.getReturnValue().setBaselineMaxOperations(() -> mekanismtweaks$baselineMaxOperations);
+    }
+
+    @Override
+    public void recalculateUpgrades(Upgrade upgrade) {
+        super.recalculateUpgrades(upgrade);
+        mekanismtweaks$baselineMaxOperations = Utils.setBaselineMaxOperation(200, this, upgrade);
+    }
+}
