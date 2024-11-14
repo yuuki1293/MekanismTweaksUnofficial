56d04131ad31301f8104091a3195f2a386453685
yuuki1293 <yuuki1293@outlook.jp>
2024-11-14 14:10:08 +0900

    expand max stack size

--- src/main/java/yuuki1293/mekanismtweaks/Config.java
+++ src/main/java/yuuki1293/mekanismtweaks/Config.java
@@ -8,17 +8,16 @@ import net.minecraftforge.fml.event.config.ModConfigEvent;
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Forge's config APIs
 @Mod.EventBusSubscriber(modid = MekanismTweaks.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class Config
-{
+public class Config {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
     private static final ForgeConfigSpec.IntValue SPEED_UPGRADE_STACK_SIZE = BUILDER
         .comment("Speed Upgrade max stack size")
-        .defineInRange("speedUpgradeStackSize", 64, 8, 64);
+        .defineInRange("speedUpgradeStackSize", 64, 8, Integer.MAX_VALUE);
 
     private static final ForgeConfigSpec.IntValue ENERGY_UPGRADE_STACK_SIZE = BUILDER
         .comment("Energy Upgrade max stack size")
-        .defineInRange("energyUpgradeStackSize", 64, 8, 64);
+        .defineInRange("energyUpgradeStackSize", 64, 8, Integer.MAX_VALUE);
 
     static final ForgeConfigSpec SPEC = BUILDER.build();
 
@@ -26,8 +25,7 @@ public class Config
     public static int energyStackSize;
 
     @SubscribeEvent
-    static void onLoad(final ModConfigEvent event)
-    {
+    static void onLoad(final ModConfigEvent event) {
         speedStackSize = SPEED_UPGRADE_STACK_SIZE.get();
         energyStackSize = ENERGY_UPGRADE_STACK_SIZE.get();
     }
