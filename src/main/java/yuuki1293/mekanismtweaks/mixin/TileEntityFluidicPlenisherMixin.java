62786b9f2162508510f7f21ef0b3cb28a338144c
yuuki1293 <yuuki1293@outlook.jp>
2024-11-14 22:12:55 +0900

    not depend TileEntityMekanism

--- src/main/java/yuuki1293/mekanismtweaks/mixin/TileEntityFluidicPlenisherMixin.java
+++ src/main/java/yuuki1293/mekanismtweaks/mixin/TileEntityFluidicPlenisherMixin.java
@@ -1,45 +1,46 @@
 package yuuki1293.mekanismtweaks.mixin;
 
 import mekanism.api.Upgrade;
-import mekanism.api.providers.IBlockProvider;
 import mekanism.common.config.MekanismConfig;
-import mekanism.common.tile.base.TileEntityMekanism;
+import mekanism.common.tile.interfaces.IUpgradeTile;
 import mekanism.common.tile.machine.TileEntityFluidicPlenisher;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import yuuki1293.mekanismtweaks.Utils;
 
+import java.lang.reflect.InvocationTargetException;
+
 @Mixin(value = TileEntityFluidicPlenisher.class, remap = false)
-public class TileEntityFluidicPlenisherMixin extends TileEntityMekanism {
+public abstract class TileEntityFluidicPlenisherMixin implements IUpgradeTile {
+    @Shadow
+    @Final
+    public static int BASE_TICKS_REQUIRED;
     @Unique
     private int mekanismtweaks$baselineMaxOperations = 1;
 
     @Unique
     private Boolean mekanismtweaks$lock = false;
 
-    public TileEntityFluidicPlenisherMixin(IBlockProvider blockProvider, BlockPos pos, BlockState state) {
-        super(blockProvider, pos, state);
-    }
-
     @Inject(method = "onUpdateServer", at = @At(value = "TAIL"))
-    private void onUpdateServer(CallbackInfo ci) {
+    private void onUpdateServer(CallbackInfo ci) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         if (mekanismtweaks$lock) return;
 
         mekanismtweaks$lock = true;
         var count = mekanismtweaks$baselineMaxOperations;
         for (int i = 1; i < count; i++) {
-            this.onUpdateServer();
+            var clazz = this.getClass();
+            clazz.getDeclaredMethod("onUpdateServer").invoke(this);
         }
         mekanismtweaks$lock = false;
     }
 
     @Inject(method = "recalculateUpgrades", at = @At(value = "TAIL"))
     private void recalculateUpgrades(Upgrade upgrade, CallbackInfo ci) {
-        Utils.setBaselineMaxOperation(MekanismConfig.general.minerTicksPerMine.get(), this, upgrade, x -> mekanismtweaks$baselineMaxOperations = x);
+        Utils.setBaselineMaxOperation(BASE_TICKS_REQUIRED, this, upgrade, x -> mekanismtweaks$baselineMaxOperations = x);
     }
 }
--- src/main/java/yuuki1293/mekanismtweaks/mixin/TileEntityFormulaicAssemblicatorMixin.java
+++ src/main/java/yuuki1293/mekanismtweaks/mixin/TileEntityFormulaicAssemblicatorMixin.java
@@ -3,6 +3,7 @@ package yuuki1293.mekanismtweaks.mixin;
 import mekanism.api.Upgrade;
 import mekanism.api.providers.IBlockProvider;
 import mekanism.common.tile.base.TileEntityMekanism;
+import mekanism.common.tile.interfaces.IUpgradeTile;
 import mekanism.common.tile.machine.TileEntityFormulaicAssemblicator;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
@@ -15,9 +16,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import yuuki1293.mekanismtweaks.Utils;
 
+import java.lang.reflect.InvocationTargetException;
+
 // Formulaic Assemblicator
 @Mixin(value = TileEntityFormulaicAssemblicator.class, remap = false)
-public abstract class TileEntityFormulaicAssemblicatorMixin extends TileEntityMekanism {
+public abstract class TileEntityFormulaicAssemblicatorMixin implements IUpgradeTile {
     @Shadow
     @Final
     private static int BASE_TICKS_REQUIRED;
@@ -26,25 +29,21 @@ public abstract class TileEntityFormulaicAssemblicatorMixin extends TileEntityMe
     @Unique
     private boolean mekanismtweaks$lock;
 
-    public TileEntityFormulaicAssemblicatorMixin(IBlockProvider blockProvider, BlockPos pos, BlockState state) {
-        super(blockProvider, pos, state);
-    }
-
     @Inject(method = "onUpdateServer", at = @At(value = "TAIL"))
-    private void onUpdateServer(CallbackInfo ci) {
+    private void onUpdateServer(CallbackInfo ci) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         if (mekanismtweaks$lock) return;
 
         mekanismtweaks$lock = true;
         var count = mekanismtweaks$baselineMaxOperations;
         for (int i = 1; i < count; i++) {
-            this.onUpdateServer();
+            var clazz = this.getClass();
+            clazz.getDeclaredMethod("onUpdateServer").invoke(this);
         }
         mekanismtweaks$lock = false;
     }
 
     @Inject(method = "recalculateUpgrades", at = @At(value = "HEAD"))
     public void recalculateUpgrades(Upgrade upgrade, CallbackInfo ci) {
-        super.recalculateUpgrades(upgrade);
         Utils.setBaselineMaxOperation(BASE_TICKS_REQUIRED, this, upgrade, x -> mekanismtweaks$baselineMaxOperations = x);
     }
 }
